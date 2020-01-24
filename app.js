5a6c410625c18fd8c1bd2816a28cebfd0220ae7d
DanG <gallagherd07@gmail.com>
2020-01-24 17:36:49 -0600

    Update checkpoint 12

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
 function onReady() {
-  const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
-  addToDoForm.addEventListener('submit', () => {
+  const ADD_TODO_FORM = document.getElementById('addToDoForm');
+  const NEW_TODO_TEXT = document.getElementById('newToDoText');
+  const TODO_LIST = document.getElementById('toDoList');
+
+  ADD_TODO_FORM.addEventListener('submit', (event) => {
     event.preventDefault();
+
     //get the text
-    let title = newToDoText.value;
+    let title = NEW_TODO_TEXT.value;
+
     //create a new li
     let newLi = document.createElement('li');
+
     //create a new input
     let checkbox = document.createElement('input');
+
     //set the input's type to checkbox
     checkbox.type = "checkbox";
+
     //set the title
     newLi.textContent = title;
+
     //attach the checkbox to the li
-    newLi.appendChild(checkbox)
+    newLi.appendChild(checkbox);
+
     //attach the li to the ul
-    toDoList.appendChild(newLi);
+    TODO_LIST.appendChild(newLi);
+
     //empty the input
-    new.ToDoText.value = '';
+    NEW_TODO_TEXT.value = '';
   });
 }
 
