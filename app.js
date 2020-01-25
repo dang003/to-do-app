52197a70e462f2d2af81ab22a3d976ac9f94f1a4
DanG <gallagherd07@gmail.com>
2020-01-24 21:06:54 -0600

    Assignment 12 completed

--- app.js
+++ app.js
@@ -11,9 +11,17 @@ function onReady() {
 
     //create a new li
     let newLi = document.createElement('li');
+    newLi.classList.add("mdl-list__item");
 
     //create a new input
     let checkbox = document.createElement('input');
+
+    let deleteButton = document.createElement('button');
+    deleteButton.textContent = "Delete";
+
+    deleteButton.addEventListener('click', function(event) {
+      TODO_LIST.removeChild(this.parentElement);
+    })
 
     //set the input's type to checkbox
     checkbox.type = "checkbox";
@@ -23,6 +31,9 @@ function onReady() {
 
     //attach the checkbox to the li
     newLi.appendChild(checkbox);
+
+    //attach delete to the li
+    newLi.appendChild(deleteButton);
 
     //attach the li to the ul
     TODO_LIST.appendChild(newLi);
